52336085c8ac9ffe00dea8607009d3024b096437
Walid <83774096+ziliwalid@users.noreply.github.com>
2024-05-16 22:24:30 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,45 @@
 #!/usr/bin/python3
 
-"""reads stdin lines"""
+"""Script that reads stdin line by line and computes metrics"""
 
 import sys
 
 
-def printStats(dic, size):
-    """ prints stats"""
+def printsts(dic, size):
+    """ WWPrints information """
     print("File size: {:d}".format(size))
     for i in sorted(dic.keys()):
         if dic[i] != 0:
             print("{}: {:d}".format(i, dic[i]))
 
 
-stats = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
        "404": 0, "405": 0, "500": 0}
 
-countStat = 0
-length = 0
+count = 0
+size = 0
 
 try:
     for line in sys.stdin:
-        if countStat != 0 and countStat % 10 == 0:
-            printStats(stats, length)
+        if count != 0 and count % 10 == 0:
+            printsts(sts, size)
 
         stlist = line.split()
-        countStat += 1
+        count += 1
 
         try:
-            length += int(stlist[-1])
+            size += int(stlist[-1])
         except:
             pass
 
         try:
-            if stlist[-2] in stats:
-                stats[stlist[-2]] += 1
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
         except:
             pass
-    printStats(stats, length)
+    printsts(sts, size)
+
 
 except KeyboardInterrupt:
-    printStats(stats, length)
+    printsts(sts, size)
     raise
